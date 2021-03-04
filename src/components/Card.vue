7afce3c3a812a19f76c23e0fc25da79df5edca31
Florent Bouisset <florent.bouisset@gmail.com>
2021-03-04 23:13:55 +0100

    add imgs

---
+++ src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-img
+      max-height="150"
+      max-width="250"
+      :src="require('../assets/diamondsKing.png')"
+    ></v-img>
+  </div>
+</template>
+
+<script>
+const colors = {
+  CLUB: "club",
+  DIAMOND: "diamond",
+  HEART: "heart",
+  SPADE: "spade",
+};
+
+export default {
+  data: function () {
+    return {
+      color: colors.CLUB,
+    };
+  },
+  computed: {
+    imagePath() {
+      const baseSrc = "@/assets/colors/";
+      switch (this.color) {
+        case colors.CLUB:
+          return baseSrc + "club.png";
+        default:
+          return null;
+      }
+    },
+  },
+};
+</script>
